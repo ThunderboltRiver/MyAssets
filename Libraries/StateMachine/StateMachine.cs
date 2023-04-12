579ffeace81684f6966a5f5edc7aa79cb7579a89
ThunderboltRiver <lostbleuseas@outlook.jp>
2023-04-13 00:15:57 +0900

    StateMachineのジェネリックなStartメソッドの追加とそのテスト

--- Libraries/StateMachine/StateMachine.cs
+++ Libraries/StateMachine/StateMachine.cs
@@ -8,18 +8,61 @@ namespace StateMachine
 {
     public class StateMachine<TOwner>
     {
-        private Dictionary<int, Dictionary<State, State>> _transitions = new();
+        private Dictionary<State, Dictionary<int, State>> _transitions = new();
 
+        public abstract class State
+        {
+            internal StateMachine<TOwner> stateMachine;
+            public State() { }
+            protected internal virtual void OnEnter() { }
+            public static bool operator ==(State a, State b)
+            {
+                return a.GetType() == b.GetType();
+            }
+            public static bool operator !=(State a, State b)
+            {
+                return a.GetType() != b.GetType();
+            }
+            public override bool Equals(object obj)
+            {
+                return GetType() == obj.GetType();
+            }
+
+            public override int GetHashCode()
+            {
+                return GetType().GetHashCode();
+            }
+
+        }
+        private class EmptyState : State { }
         public TOwner Owner { get; }
-        public State CurrentState { get; private set; }
+        public State CurrentState { get; private set; } = new EmptyState();
         public StateMachine(TOwner owner)
         {
             Owner = owner;
         }
+
+        public T RegisterOrGetState<T>() where T : State, new()
+        {
+            var wantStates = _transitions.Keys.Where(x => x is T);
+            if (wantStates.Count() == 0)
+            {
+                var state = new T();
+                _transitions.Add(state, new Dictionary<int, State>());
+                return state;
+            }
+            return wantStates.First() as T;
+        }
+
         public void Start<T>() where T : State, new()
         {
-
+            if (CurrentState is not EmptyState)
+            {
+                throw new InvalidOperationException("State is already started.");
+            }
+            CurrentState = RegisterOrGetState<T>();
         }
+
         public void AddTransition<T1, T2>(int eventKey) where T1 : State, new() where T2 : State, new()
         {
         }
--- Libraries/StateMachine/Tests/StateMachineUnitTest.cs
+++ Libraries/StateMachine/Tests/StateMachineUnitTest.cs
@@ -1,11 +1,10 @@
 using NUnit.Framework;
-
+using State = StateMachine.StateMachine<object>.State;
 namespace StateMachine
 {
     public class StateMachineUnitTest
     {
         // A Test behaves as an ordinary method
-
         public class State1 : State
         {
         }
@@ -13,6 +12,10 @@ namespace StateMachine
         public class State2 : State
         {
         }
+        public class State3 : State
+        {
+        }
+
 
         [Test]
         public void StateMachine_State1で開始するとeventKeyが1を発行されたときにState2に遷移する()
@@ -26,6 +29,41 @@ namespace StateMachine
             stateMachine.DispatchEvent(eventKey);
             bool didEndState2 = stateMachine.CurrentState is State2;
             Assert.That(didStartState1 && didEndState2, Is.True);
+        }
+
+        [Test]
+        public void StateMachine_State1で開始するとeventKeyが2を発行されたときにState2に遷移しない()
+        {
+            object player = new();
+            var stateMachine = new StateMachine<object>(player);
+            var eventKey = 1;
+            stateMachine.AddTransition<State1, State2>(eventKey);
+            stateMachine.Start<State1>();
+            bool didStartState1 = stateMachine.CurrentState is State1;
+            stateMachine.DispatchEvent(2);
+            bool didEndState2 = stateMachine.CurrentState is State2;
+            Assert.That(didStartState1 && didEndState2, Is.False);
+        }
+        [Test]
+        public void StateMachine_AddTransition_すでにState1から同じeventKeyでの遷移が登録されている場合は新しい遷移で上書きされる()
+        {
+            object player = new();
+            var stateMachine = new StateMachine<object>(player);
+            var eventKey = 1;
+            stateMachine.AddTransition<State1, State2>(eventKey);
+            bool isBeforeState2 = stateMachine.CurrentState == new State2();
+            stateMachine.AddTransition<State1, State3>(eventKey);
+            bool isAfterState3 = stateMachine.CurrentState == new State3();
+            Assert.That(isBeforeState2 && isAfterState3, Is.True);
+        }
+
+        [Test]
+        public void StateMachine_Start_State1で開始するとState1になる()
+        {
+            object player = new();
+            var stateMachine = new StateMachine<object>(player);
+            stateMachine.Start<State1>();
+            Assert.That(stateMachine.CurrentState, Is.EqualTo(new State1()));
         }
     }
 }
