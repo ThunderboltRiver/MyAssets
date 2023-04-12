e252fbb97e40457a0a6553c0fcbbadea8f3fab5f
ThunderboltRiver <lostbleuseas@outlook.jp>
2023-04-13 00:37:10 +0900

    StateMachineのStartメソッドの整形

--- Libraries/StateMachine/StateMachine.cs
+++ Libraries/StateMachine/StateMachine.cs
@@ -41,19 +41,24 @@ namespace StateMachine
         {
             Owner = owner;
         }
-
-        public T RegisterOrGetState<T>() where T : State, new()
+        /// <summary>
+        /// 既に登録されているStateならそれを返す。なければ新しく登録してから返す。
+        /// </summary>
+        /// <typeparam name="T">登録したい具象Stateの型</typeparam>
+        /// <returns>登録された具象Stateのインスタンス</returns>
+        private T RegisterOrGetState<T>() where T : State, new()
         {
-            var wantStates = _transitions.Keys.Where(x => x is T);
-            if (wantStates.Count() == 0)
-            {
-                var state = new T();
-                _transitions.Add(state, new Dictionary<int, State>());
-                return state;
-            }
-            return wantStates.First() as T;
+            var state = new T();
+            var wantStates = _transitions.Keys.Where(x => x == state);
+            return _transitions.TryAdd(state, new Dictionary<int, State>()) ? state : wantStates.First() as T;
         }
 
+        /// <summary>
+        /// StateMachineを開始する。すでに開始されている場合は例外を投げる。
+        /// </summary>
+        /// <typeparam name="T">開始する具象Stateの型</typeparam>
+        /// <exception cref="InvalidOperationException">すでに開始されている場合</exception>
+        /// <exception cref="ArgumentException">引数の型が具象Stateでない場合</exception>
         public void Start<T>() where T : State, new()
         {
             if (CurrentState is not EmptyState)
