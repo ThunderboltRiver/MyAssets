fad5eec49befd7ed075a6dfb2d116486a7063cd4
ThunderboltRiver <lostbleuseas@outlook.jp>
2023-05-20 21:12:44 +0900

    GameProgressMachineの作成

---
+++ Libraries/SceenChangeSystem/src/ProgressMachine.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+namespace ScreenChangeSystem
+{
+    public class ProgressMachine : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        public IObservable<string> CurrentGameProgress { get; private set; }
+
+        public void AddStartProgress(GameProgress gameProgress, int progressKey, GameProgress gameProgress)
+        {
+        }
+
+        public void ChangeGameProgress(int progressKey)
+        {
+        }
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
